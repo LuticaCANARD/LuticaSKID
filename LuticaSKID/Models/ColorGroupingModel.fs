10ec1a61986dbdd52afec814873ff92b67fed180
Lutica_CANARD <presan100@gmail.com>
2025-04-23 01:57:22 +0900

    클러스터링 기능 개선: 관성 계산 및 최적 K 찾기
    `ColorGroupingModel.fs` 파일에 `computeInertia` 및 `findBestK` 메서드를 추가하여 색상 그룹의 응집력을 측정하고 최적의 클러스터 수를 찾는 기능을 구현했습니다.
    
    `ExecuteKmeans` 메서드를 수정하여 고정된 K 값 대신 최대 K 값까지 반복하여 클러스터 중심을 업데이트하고 관성을 계산하여 클러스터링 품질을 평가합니다.

--- LuticaSKID/Models/ColorGroupingModel.fs
+++ LuticaSKID/Models/ColorGroupingModel.fs
@@ -20,6 +20,42 @@ module ColorGroupingModel =
     type ColorGroupiongAnlyzeResult = AnalyzeResult<ColorGroupingResult>
 
     type Process() =
+        static member computeInertia (assignments: int[]) (centroids: SKIDColor[]) (image: SKIDImage) =
+            assignments
+            |> Array.mapi (fun i cid ->
+                let p = image.pixels.[i]
+                let c = centroids.[cid]
+                let dr = p.r- c.r
+                let dg = p.g - c.g
+                let db = p.b - c.b
+                dr * dr + dg * dg + db * db)
+            |> Array.sum
+                    
+        static member findBestK (image:SKIDImage) (maxK:int):ColorGroupiongAnlyzeResult =
+            let inertias = ResizeArray()
+            let results = ResizeArray()
+            let mutable bestK:ColorGroupiongAnlyzeResult = {
+                result = { colorResult = []; colorCount = 0 }
+            }
+            for k in 1 .. maxK do
+                let result = Process.ExecuteKmeans image
+                let assignments = result.result.colorResult |> Array.ofList |> Array.Parallel.map(fun a->a.colorElement) 
+                let centroids = 
+                    assignments 
+                    |> Array.distinct
+                    |> Array.distinctBy id
+                let inertia = Process.computeInertia (assignments |> Array.Parallel.mapi (fun i c -> Array.findIndex ((=) c) centroids)) centroids image
+                inertias.Add(inertia)
+                results.Add((k, result, inertia))
+                if inertias.Count > 2 then
+                    let last = inertias.[inertias.Count - 1]
+                    let prev = inertias.[inertias.Count - 2]
+                    if (prev - last) / prev < 0.05f then // inertia 감소폭이 5% 미만이면 종료
+                        bestK  <- results |> Seq.minBy (fun (k, r, inertia) ->  k + int(inertia * 0.001f)) |> (fun (_, r, _) -> r)
+            bestK
+
+                        
+            
         static member kmeansKernel (tid:Index1D) 
             (pixels:ArrayView<SKIDColor>) 
             (centroids:ArrayView<SKIDColor>) 
@@ -46,60 +82,81 @@ module ColorGroupingModel =
 
                     assignments.[tid] <- best
             
-        static member ExecuteKmeans (image:SKIDImage) : ColorGroupiongAnlyzeResult = 
+        static member ExecuteKmeans (image:SKIDImage) : ColorGroupiongAnlyzeResult =
             use context = Context.CreateDefault()
             use accelerator = context.GetPreferredDevice(preferCPU=false).CreateAccelerator(context)
 
             let kernel = accelerator.LoadAutoGroupedStreamKernel<Index1D,ArrayView<SKIDColor>,ArrayView<SKIDColor>,ArrayView<int>,int,int> Process.kmeansKernel
-            
-            // ---------------------
-            let k = 8
-            let centroids = Array.create k (SKIDColor(0.0f, 0.0f, 0.0f, 0.0f))
-            let assignments = Array.create image.pixels.Length 0
-            // 이전값. 
-            let mutable hostCentroids = Array.init (k) (fun i -> centroids.[i]) 
-            let mutable hostAssignments = Array.init (image.pixels.Length) (fun i -> assignments.[i])
+
+            let maxK = 10
+            let pixelLength = image.pixels.Length
+
+            let mutable bestResult = Unchecked.defaultof<_>
+            let mutable previousInertia = Single.MaxValue
+            let mutable bestK = 2
+            let mutable diff = Single.MaxValue
+            for k in 2 .. maxK do
+                if diff > 0.05f then
+                    let centroids = Array.init k (fun _ -> SKIDColor(0.0f, 0.0f, 0.0f, 1.0f))
+                    let assignments = Array.zeroCreate pixelLength
+                    use d_centroids = accelerator.Allocate1D<SKIDColor>(centroids)
+                    use d_originImage = accelerator.Allocate1D<SKIDColor>(image.pixels)
+                    use d_assignments = accelerator.Allocate1D<int>(assignments.Length)
+
+                    for _ in 0 .. 9 do
+                        kernel.Invoke(d_originImage.IntExtent, d_originImage.View, d_centroids.View, d_assignments.View, k, pixelLength)
+                        accelerator.Synchronize()
+
+                        let hostAssignments = d_assignments.GetAsArray1D()
+
+                        // centroids 업데이트
+                        let centroidSums = Array.init k (fun _ -> (0.0f, 0.0f, 0.0f, 0))
+                        for j in 0 .. pixelLength - 1 do
+                            let clusterIndex = hostAssignments.[j]
+                            let p = image.pixels.[j]
+                            let (rSum, gSum, bSum, count) = centroidSums.[clusterIndex]
+                            centroidSums.[clusterIndex] <- (rSum + p.r, gSum + p.g, bSum + p.b, count + 1)
+
+                        for i in 0 .. k - 1 do
+                            let (rSum, gSum, bSum, count) = centroidSums.[i]
+                            if count > 0 then
+                                centroids.[i] <- SKIDColor(rSum / float32 count, gSum / float32 count, bSum / float32 count, 1.0f)
+
+                        d_centroids.CopyFromCPU(centroids)
 
 
+                    let finalCentroids = d_centroids.GetAsArray1D()
+                    let finalAssignments = d_assignments.GetAsArray1D()
 
-            // --------------------- Assignments on GPU...
-            use d_centroids = accelerator.Allocate1D<SKIDColor>(centroids)
-            use d_originImage = accelerator.Allocate1D<SKIDColor>(image.pixels)
-            use d_assignments = accelerator.Allocate1D<int>(assignments.Length)
+                    // inertia 계산
+                    let inertia =
+                        Array.Parallel.sumBy (fun i ->
+                            let p = image.pixels.[i]
+                            let c = finalCentroids.[finalAssignments.[i]]
+                            let dr = p.r - c.r
+                            let dg = p.g - c.g
+                            let db = p.b - c.b
+                            dr*dr + dg*dg + db*db
+                        ) [| 0 .. pixelLength - 1 |]
+                    let colorResult =
+                        finalCentroids
+                        |> Array.Parallel.mapi (fun i c ->
+                            let count = finalAssignments |> Array.Parallel.filter ((=) i) |> Array.length
+                            { colorElement = c; weight = float32 count / float32 pixelLength; piexelcount = count }
+                        ) |> Array.toList
 
-            //--------------------  
+                    bestResult <- {
+                        result = { colorResult = colorResult; colorCount = finalCentroids.Length };
+                    }
+                    // inertia 감소가 5% 미만이면 멈춤
+                    if previousInertia <> Single.MaxValue then
+                        diff <- (previousInertia - inertia) / previousInertia
+                        if diff > 0.05f then
+                            previousInertia <- inertia
+                            bestK <- k
 
-            for _ in 0 .. 10 do
-                kernel.Invoke(d_originImage.IntExtent, d_originImage.View, d_centroids.View, d_assignments.View, k, image.pixels.Length)
-                accelerator.Synchronize()
-                // Copy back to host
-                hostCentroids <- d_centroids.GetAsArray1D()
-                hostAssignments <- d_assignments.GetAsArray1D()
-                // Update centroids
-                for i in 0 .. k - 1 do
-                    let mutable r = 0.0f
-                    let mutable g = 0.0f
-                    let mutable b = 0.0f
-                    let mutable count = 0
-                    for j in 0 .. image.pixels.Length - 1 do
-                        if hostAssignments.[j] = i then
-                            r <- r + image.pixels.[j].r
-                            g <- g + image.pixels.[j].g
-                            b <- b + image.pixels.[j].b
-                            count <- count + 1
-                    if count > 0 then
-                        centroids.[i] <- SKIDColor(r / float32 count, g / float32 count, b / float32 count, 1.0f)
-            let finalCentroids = d_centroids.GetAsArray1D()
-            let finalAssignments = d_assignments.GetAsArray1D()
-            let colorResult = 
-                finalCentroids
-                |> Array.mapi (fun i c -> 
-                    let count = Array.filter (fun x -> x = i) finalAssignments |> Array.length
-                    {colorElement = c; weight = float32 count / float32 image.pixels.Length; piexelcount = count}
-                ) |> Array.toList
-            {
-                result = { colorResult = colorResult; colorCount = finalCentroids.Length };
-            }
+            bestResult
+
 
 
             
